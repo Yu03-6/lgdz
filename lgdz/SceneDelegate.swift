6c475aa3f28b89f93c52e6bc06a116ab41b8341d
Xinlin <i22022971@student.newinti.edu.my>
2026-06-15 15:11:35 +0800

    1
    1

--- lgdz/SceneDelegate.swift
+++ lgdz/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Mitabe
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -20,6 +21,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window.rootViewController = AppRouter.shared.makeRoot()
         window.makeKeyAndVisible()
         self.window = window
+        
+        Mazimonu.hodoxu()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
